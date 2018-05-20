f09c54020d98f7daf6d4e11dd4c169616eed4da8
jay-shah <jay_shah_14@live.co.uk>
2018-05-20 20:06:47 +0100

    Fixed playing and pausing issue

--- audioPlay.js
+++ audioPlay.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,12 +11,9 @@ import {
   Button
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import Sound from 'react-native-sound';
 import PlayOrPause from './playOrPause';
-
-
-
 
 
 
@@ -27,20 +24,25 @@ export default class AudioPlay extends Component {
 
     Sound.setCategory('Playback', true);
 
+    let url = this.props.navigation.state.params.url
+    let whoosh = new Sound(url, Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // loaded successfully
+      console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+    });
+
     this.state = {
-      playing: undefined,
-      isPlaying: false
+      whoosh: whoosh,
+      playing: false
     }
-
-
-
   }
 
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.number,
-    headerRight: (
-      <View></View>
-    ),
+
     headerTitleStyle: {
       textAlign: 'center',
       alignSelf: 'center'
@@ -50,92 +52,66 @@ export default class AudioPlay extends Component {
 
   componentWillUnmount() {
 
-    if (!this.state.playing) {
-      return;
+    if (this.state.whoosh) {
+      this.state.whoosh.stop().release();
     }
-
-    this.state.playing.stop().release();
   }
 
-
-
-
-  handlePlay = (whoosh) => {
-
-    if(!this.state.playing)
-      {
-
-    whoosh.play((success) => {
-    if (success) {
-      console.log('successfully finished playing');
-    } else {
-      console.log('playback failed due to audio decoding errors');
-      // reset the player to its uninitialized state (android only)
-      // this is the only option to recover after an error occured and use the player again
-      whoosh.release();
-    }
-
-    this.setState({
-      isPlaying: false
-    })
-    });
-
-    this.setState({
-      playing: whoosh,
-      isPlaying: true
-    })
+  handlePause = () => {
+    console.log('got here')
+    this.state.whoosh.pause()
   }
 
+  handleAudio = () => {
 
-    else if(this.state.playing && !this.state.isPlaying){
-      this.state.playing.play();
+    if (this.state.whoosh && !this.state.playing) {
+      console.log(this.state.playing)
+
+      this.state.whoosh.play((success) => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+          this.state.whoosh.release();
+        }
+
+        this.setState({
+          playing: false
+        })
+      });
+
       this.setState({
-        isPlaying: true
+        playing: true
       })
     }
 
-    else if(this.state.isPlaying){
-      this.state.playing.pause();
+    else if (this.state.playing) {
+      this.state.whoosh.pause();
       this.setState({
-        isPlaying: false
+        playing: false
       })
     }
   }
 
   render() {
 
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     let url = this.props.navigation.state.params.url
     let title = this.props.navigation.state.params.title
     let image = this.props.navigation.state.params.image
     let zone = this.props.navigation.state.params.zone
-
-    var whoosh = new Sound(url, Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-      console.log('failed to load the sound', error);
-      return;
-    }
-
-
-
-    // loaded successfully
-    console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
-  });
-
-
-
 
     return (
 
       <View style={styles.container}>
         <View style={styles.imageContainer}>
 
-          <Image source={image}  resizeMode={'cover'} style={styles.imageContainer} blurRadius={20} opacity={0.8}>
-            <LinearGradient style={styles.linearGradient} colors={["transparent", "white"]}  start={{x: 0.5, y: 0}} end={{x: 0.5, y: 1}}>
+          <Image source={image} resizeMode={'cover'} style={styles.imageContainer} blurRadius={20} opacity={0.8}>
+            <LinearGradient style={styles.linearGradient} colors={["transparent", "white"]} start={{ x: 0.5, y: 0 }} end={{ x: 0.5, y: 1 }}>
 
-          <Image source={image} resizeMode={'cover'} borderRadius={15} style={styles.imageStyle}></Image>
-</LinearGradient>
-        </Image>
+              <Image source={image} resizeMode={'cover'} borderRadius={15} style={styles.imageStyle}></Image>
+            </LinearGradient>
+          </Image>
 
 
         </View>
@@ -150,9 +126,11 @@ export default class AudioPlay extends Component {
           justifyContent: 'center',
           alignItems: 'center'
         }}>
-          <TouchableOpacity onPress={() => {this.handlePlay(whoosh)}}>
-            <PlayOrPause isPlaying={this.state.isPlaying} />
+
+          <TouchableOpacity onPress={this.handleAudio}>
+            <PlayOrPause playing={this.state.playing} />
           </TouchableOpacity>
+
 
         </View>
       </View>
@@ -178,7 +156,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    width: Dimensions.get('window').width / 1.5 ,
+    width: Dimensions.get('window').width / 1.5,
     height: Dimensions.get('window').width / 1.5 - 20
   },
   titleContainer: {
@@ -201,10 +179,10 @@ const styles = StyleSheet.create({
     fontWeight: 'normal'
   },
   linearGradient: {
-   flex: 1,
-   width: Dimensions.get('window').width,
-   justifyContent: 'center',
-   alignItems: 'center'
- }
+    flex: 1,
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 
 });
